d4aa67d8290e0cd3ee9bd988f3e60ac8b3e151f2
Dan Adrian Mirea <damirea84@gmail.com>
2024-07-05 04:44:33 +0300

    first commit

---
+++ init.bat
@@ -0,0 +1,7 @@
+echo "# zighelloworld" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:danadrianmirea/zighelloworld.git
+git push -u origin main
